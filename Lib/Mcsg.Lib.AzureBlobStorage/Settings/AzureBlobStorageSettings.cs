18bca9179eb63863c911802450cbe260a860bce5
ToanNV (home) <nvt87x@gmail.com>
2024-06-15 12:11:30 +0700

    Remove Mcsg.Lib.AzureBlobStorage

--- Lib/Mcsg.Lib.AzureBlobStorage/Settings/AzureBlobStorageSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Mcsg.Lib.AzureBlobStorage.Settings
-{
-    public class AzureBlobStorageSettings
-    {
-        public string StorageName { get; set; }
-        public string AccountKey { get; set; }
-    }
-}
