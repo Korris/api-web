1c6e50a0584b369304e25e563b2b8a0297ab6ca0
ToanNV <toannguyen@angelpj.com>
2024-06-27 10:40:55 +0700

    Allow null values

--- Mcsg.Realtime.Api/DTOs/VideoNotificationReq.cs
+++ Mcsg.Realtime.Api/DTOs/VideoNotificationReq.cs
@@ -6,12 +6,12 @@ namespace Mcsg.Realtime.Api.DTOs
     {
         public Guid Id { get; set; }
         public NotificationAction Action { get; set; }
-        public string HashId { get; set; }
+        public string? HashId { get; set; }
         public Guid? PostId { get; set; }
-        public string PostHashId { get; set; }
-        public string TargetType { get; set; }
+        public string? PostHashId { get; set; }
+        public string? TargetType { get; set; }
         public Guid AuthorId { get; set; }
-        public string AuthorName { get; set; }
-        public string UserAvatar { get; set; }
+        public string? AuthorName { get; set; }
+        public string? UserAvatar { get; set; }
     }
 }
