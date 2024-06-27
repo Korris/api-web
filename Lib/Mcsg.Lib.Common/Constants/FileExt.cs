2b4df075c0ec298be95ca16d5b24c0001301b1b7
KietNTT <toannguyen@angelpj.com>
2024-06-27 10:40:30 +0700

    BUMCHEO-227 Image format for iPhone API

--- Lib/Mcsg.Lib.Common/Constants/FileExt.cs
+++ Lib/Mcsg.Lib.Common/Constants/FileExt.cs
@@ -3,7 +3,7 @@
     public static class FileExt
     {
         public static string[] Audios = { ".mp3", ".wav", ".ogg" };
-        public static string[] Images = { ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".heic", ".webp" };
+        public static string[] Images = { ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".heic", ".webp", ".heif" };
         public static string[] Videos = { ".mp4", ".avi", ".mkv", ".mov", ".webm" };
         public static string[] AllowPlayAfterUpload = { ".mp4", ".webm", ".ogg", ".mp3" };
     }
