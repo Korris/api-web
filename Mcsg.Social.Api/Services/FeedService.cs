f785a3a838fa4cd493d4e9bea5503679d617f212
TanPM <korristan@angelpj.com>
2024-07-01 09:52:35 +0700

    hot fix save post data

--- Mcsg.Social.Api/Services/FeedService.cs
+++ Mcsg.Social.Api/Services/FeedService.cs
@@ -519,6 +519,7 @@ namespace Mcsg.Social.Api.Services
             };
             try
             {
+                await _postRepository.InsertAsync(post);
                 if (feedPostReq.MetaData != null)
                 {
                     feedPostReq.MetaData.Description = System.Web.HttpUtility.HtmlEncode(feedPostReq.MetaData.Description);
@@ -556,7 +557,6 @@ namespace Mcsg.Social.Api.Services
                         result.Link = new PostLinkResponse();
                     }
                 }
-                await _postRepository.InsertAsync(post);
             }
             catch (PostgresException ex)
             {
