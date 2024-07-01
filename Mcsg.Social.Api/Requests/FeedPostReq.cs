4099fb4dc5dd1ae65524f002ff4764d0391f187c
TanPM <korristan@angelpj.com>
2024-07-01 16:21:50 +0700

    turn off require data when post feed

--- Mcsg.Social.Api/Requests/FeedPostReq.cs
+++ Mcsg.Social.Api/Requests/FeedPostReq.cs
@@ -5,9 +5,9 @@
         public string? Title { get; set; }
         public string? Content { get; set; }
         public string? ThumbnailUrl { get; set; }
-        public MetaDataReq MetaData { get; set; }
-        public List<string> Tags { get; set; }
-        public List<ResourcePostReq> Files { get; set; }
+        public MetaDataReq? MetaData { get; set; }
+        public List<string>? Tags { get; set; }
+        public List<ResourcePostReq>? Files { get; set; }
         public Guid? SoundId { get; set; }
         public string? CustomNote { get; set; }
     }
