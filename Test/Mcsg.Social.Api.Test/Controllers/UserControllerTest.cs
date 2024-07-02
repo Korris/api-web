f34057e8c451c42c6e6292d9d4c42f6d66438df1
ToanNV <toannguyen@angelpj.com>
2024-07-02 19:39:20 +0700

    Update PackageReference

--- Test/Mcsg.Social.Api.Test/Controllers/UserControllerTest.cs
+++ Test/Mcsg.Social.Api.Test/Controllers/UserControllerTest.cs
@@ -12,42 +12,42 @@ public class UserControllerTest
     }
 
     [Test]
-    public async Task UpdateUserProfile_01_ValidationFailed()
+    public void UpdateUserProfile_01_ValidationFailed()
     {
         var req = new UserProfileUpdateR { ProfileName = "abc" };
         var vr = new UserProfileUpdateV().Validate(req);
-        Assert.IsFalse(vr.IsValid);
+        Assert.That(vr.IsValid, Is.EqualTo(false));
 
         req = new UserProfileUpdateR { ProfileName = "" };
         vr = new UserProfileUpdateV().Validate(req);
-        Assert.IsFalse(vr.IsValid);
+        Assert.That(vr.IsValid, Is.EqualTo(false));
 
         req = new UserProfileUpdateR { ProfileName = null };
         vr = new UserProfileUpdateV().Validate(req);
-        Assert.IsFalse(vr.IsValid);
+        Assert.That(vr.IsValid, Is.EqualTo(false));
     }
 
     [Test]
-    public async Task UpdateUserProfile_02_ValidationFailed()
+    public void UpdateUserProfile_02_ValidationFailed()
     {
         var req = new UserProfileUpdateR { ProfileName = "$$$%%@#" };
         var vr = new UserProfileUpdateV().Validate(req);
-        Assert.IsFalse(vr.IsValid);
+        Assert.That(vr.IsValid, Is.EqualTo(false));
 
         req = new UserProfileUpdateR { ProfileName = "xxxxx" };
         vr = new UserProfileUpdateV().Validate(req);
-        Assert.IsFalse(vr.IsValid);
+        Assert.That(vr.IsValid, Is.EqualTo(false));
     }
 
     [Test]
-    public async Task UpdateUserProfile_01_ValidationPassed()
+    public void UpdateUserProfile_01_ValidationPassed()
     {
         var req = new UserProfileUpdateR { ProfileName = "__333x" };
         var vr = new UserProfileUpdateV().Validate(req);
-        Assert.IsFalse(vr.IsValid);
+        Assert.That(vr.IsValid, Is.EqualTo(true));
 
         req = new UserProfileUpdateR { ProfileName = "tpaaa fsfadsf" };
         vr = new UserProfileUpdateV().Validate(req);
-        Assert.IsFalse(vr.IsValid);
+        Assert.That(vr.IsValid, Is.EqualTo(true));
     }
 }
