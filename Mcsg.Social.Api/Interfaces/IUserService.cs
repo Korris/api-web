a02a7f7da8c727867302d35c4277e12b747ecc13
ToanNV <toannguyen@angelpj.com>
2024-07-08 14:22:06 +0700

    Refactor namespace

--- Mcsg.Social.Api/Interfaces/IUserService.cs
+++ Mcsg.Social.Api/Interfaces/IUserService.cs
@@ -1,26 +1,25 @@
 ﻿using Mcsg.Lib.Common.Enums;
 
-namespace Mcsg.Social.Api.Interfaces
-{
-    using Lib.Data.Entities.Common;
-    using Models;
-    using Requests;
+namespace Mcsg.Social.Api.Interfaces;
 
-    public interface IUserService
-    {
-        Task<UserProfileResponse> GetCurrentUserAsync();
-        Task<UserProfileResponse> GetUserByUserNameAsync(string profileName);
-        Task<UserProfileResponse> UpdateUserProfile(UserProfileUpdateR req);
-        Task<UserAvatarUpdateResponse> UpdateUserAvatar(UserAvatarUpdateR userAvatarUpdateRequest);
-        Task<UserProfileAvatarResponse> GetUserAvatar(Guid userId);
-        Task<UserCoverPhotoUpdateResponse> UpdateUserCoverPhoto(UserCoverPhotoUpdateR userCoverPhotoUpdateRequest);
-        Task<List<SimilarProfile>> GetSimilarNameAsync(string name);
-        Task SyncWalletUserReward(Guid userId, float point, RewardType type);
-        Task<List<SimilarProfilesMention>> GetSimilarProfilesMentionAsync(string? name);
-        Task<PagedResults<UserSearchResponse>> SearchUserbyKeyword(SmartLookupSearchUserR input);
-        Task<List<UserFollowedResponse>> GetSuggestedProfilesNotFollowedAsync();
-        Task<PagedResults<UserFollowedResponse>> GetFollowingProfilesAsync(BasePageResultR request);
-        Task<bool> FollowUserAsync(Guid userId); 
-        Task<bool> UnFollowUserAsync(Guid userId);
-    }
+using Lib.Data.Entities.Common;
+using Models;
+using Requests;
+
+public interface IUserService
+{
+    Task<UserProfileResponse> GetCurrentUserAsync();
+    Task<UserProfileResponse> GetUserByUserNameAsync(string profileName);
+    Task<UserProfileResponse> UpdateUserProfile(UserProfileUpdateR req);
+    Task<UserAvatarUpdateResponse> UpdateUserAvatar(UserAvatarUpdateR userAvatarUpdateRequest);
+    Task<UserProfileAvatarResponse> GetUserAvatar(Guid userId);
+    Task<UserCoverPhotoUpdateResponse> UpdateUserCoverPhoto(UserCoverPhotoUpdateR userCoverPhotoUpdateRequest);
+    Task<List<SimilarProfile>> GetSimilarNameAsync(string name);
+    Task SyncWalletUserReward(Guid userId, float point, RewardType type);
+    Task<List<SimilarProfilesMention>> GetSimilarProfilesMentionAsync(string? name);
+    Task<PagedResults<UserSearchResponse>> SearchUserbyKeyword(SmartLookupSearchUserR input);
+    Task<List<UserFollowedResponse>> GetSuggestedProfilesNotFollowedAsync();
+    Task<PagedResults<UserFollowedResponse>> GetFollowingProfilesAsync(BasePageResultR request);
+    Task<bool> FollowUserAsync(Guid userId);
+    Task<bool> UnFollowUserAsync(Guid userId);
 }
