10f5d846bc747e813e5d3b53bf2f09bbabdd63ae
ToanNV <toannguyen@angelpj.com>
2024-07-09 09:52:13 +0700

    Add #region Information

--- Mcsg.Social.Api/Validators/Posts/Base/PostFormBaseV.cs
+++ Mcsg.Social.Api/Validators/Posts/Base/PostFormBaseV.cs
@@ -1,4 +1,17 @@
-﻿using FluentValidation;
+﻿#region Information
+/*
+ * Author       : Toan Nguyen Van
+ * Email        : nvt87x@gmail.com
+ * Phone        : +84 345 515 010
+ * ------------------------------- *
+ * Create       : 2024-Jan-21 08:37
+ * Update       : 2024-Jan-21 08:37
+ * Checklist    : 1.0
+ * Status       : New
+ */
+#endregion
+
+using FluentValidation;
 using System.Text.RegularExpressions;
 
 namespace Mcsg.Social.Api.Validators;
