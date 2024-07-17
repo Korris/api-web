cabcf5e9d883c1dd797be86256293d20b04b5518
ToanNV <toannguyen@angelpj.com>
2024-07-17 21:54:17 +0700

    Update table name

--- Lib/Mcsg.Lib.Data/Constants/DbSchema.cs
+++ Lib/Mcsg.Lib.Data/Constants/DbSchema.cs
@@ -6,8 +6,8 @@ public class DbSchema
     public const string Default = "public.";
     public const string Identity = "identity";
     public const string Story = "story";
-    public const string ComicTables = "ComicPosts;ComicResources";
+    public const string ComicTables = "ComicPostCommentReactions;ComicPostComments;ComicPostReactions;ComicPosts;ComicResources;ComicSubPostCommentReactions;ComicSubPostComments;ComicSubPostReactions;ComicSubPosts";
     public const string IdentityTables = "Users;UserNameHistories;UserClaims;UserLogins;UserTokens;Roles;RoleClaims;UserRoles";
-    public const string StoryTables = "StoryPosts;StoryResources";
+    public const string StoryTables = "StoryPostCommentReactions;StoryPostComments;StoryPostReactions;StoryPosts;StoryResources;StorySubPostCommentReactions;StorySubPostComments;StorySubPostReactions;StorySubPosts";
     public const string SystemSettingHistories_Table = "SystemSettingHistories";
 }
