66b72aceeed78a032da6ff7057fea6f9cb198103
ToanNV (home) <nvt87x@gmail.com>
2024-07-27 21:51:20 +0700

    Add BaseV & move Dapper to Mcsg.Common.Core

---
+++ Common/Mcsg.Common.Core/Validators/BaseV.cs
@@ -0,0 +1,25 @@
+﻿#region Information
+/*
+ * Author       : Toan Nguyen Van
+ * Email        : nvt87x@gmail.com
+ * Phone        : +84 345 515 010
+ * ------------------------------- *
+ * Create       : 2024-Jan-21 08:37
+ * Update       : 2024-Jan-21 08:37
+ * Checklist    : 1.0
+ * Status       : New
+ */
+#endregion
+
+using FluentValidation;
+
+namespace Mcsg.Common.Core.Validators;
+
+using Requests;
+
+/// <summary>
+/// BaseV validator
+/// </summary>
+public class BaseV : AbstractValidator<BaseR>
+{
+}
