f46834e9c0ee6a46169a1e7951354342f9616f27
ToanNV <toannguyen@angelpj.com>
2024-08-14 09:28:04 +0700

    Remove unused code

--- Mcsg.Social.Api/Interfaces/IUserService.cs
+++ Mcsg.Social.Api/Interfaces/IUserService.cs
@@ -1,25 +1,10 @@
 ﻿namespace Mcsg.Social.Api.Interfaces;
 
-using Common.Core.Enums;
 using Common.SeedWork.Responses;
 using Models;
 using Requests;
 
 public interface IUserService
 {
-    Task<UserProfileResponse> GetCurrentUserAsync();
-    Task<UserProfileResponse> GetUserByUserNameAsync(string profileName);
-    Task<UserProfileResponse> UpdateUserProfile(UserProfileUpdateR req);
-    Task<UserAvatarUpdateResponse> UpdateUserAvatar(UserAvatarUpdateR userAvatarUpdateRequest);
-    Task<UserProfileAvatarResponse> GetUserAvatar(Guid userId);
-    Task<UserCoverPhotoUpdateResponse> UpdateUserCoverPhoto(UserCoverPhotoUpdateR userCoverPhotoUpdateRequest);
-    Task<List<SimilarProfile>> GetSimilarNameAsync(string name);
-    Task SyncWalletUserReward(Guid userId, float point, RewardType type);
-    Task<List<SimilarProfilesMention>> GetSimilarProfilesMentionAsync(string? name);
     Task<PagedResponse<UserSearchResponse>> SearchUserbyKeyword(SmartLookupSearchUserR input);
-    Task<List<UserFollowedResponse>> GetSuggestedProfilesNotFollowedAsync(string userName);
-    Task<PagedResponse<UserFollowedResponse>> GetFollowingProfilesAsync(UserNamePagingR request);
-    Task<bool> FollowUserAsync(Guid userId);
-    Task<bool> UnFollowUserAsync(Guid userId);
-    Task<PagedResponse<UserFollowedResponse>> GetFollowedProfileAsync(UserNamePagingR req);
 }
