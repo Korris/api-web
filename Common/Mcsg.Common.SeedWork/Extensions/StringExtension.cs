f3572c3734cf1526805c704fc9d29e02f1f27a67
ToanNV <toannguyen@angelpj.com>
2024-08-22 17:02:53 +0700

    Move ToSnakeCase to Mcsg.Common.SeedWork

--- Common/Mcsg.Common.SeedWork/Extensions/StringExtension.cs
+++ Common/Mcsg.Common.SeedWork/Extensions/StringExtension.cs
@@ -12,6 +12,7 @@
 #endregion
 
 using System.ComponentModel;
+using System.Globalization;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Web;
@@ -100,6 +101,73 @@ public static class StringExtension
         res = new string(arr);
 
         return res;
+    }
+
+    /// <summary>
+    /// Converts a string to snake_case
+    /// </summary>
+    /// <param name="text">The input string</param>
+    /// <returns>The snake_case version of the input string</returns>
+    public static string ToSnakeCase(this string text)
+    {
+        if (string.IsNullOrEmpty(text))
+        {
+            return text;
+        }
+
+        var builder = new StringBuilder(text.Length + Math.Min(2, text.Length / 5));
+        var previousCategory = default(UnicodeCategory?);
+
+        for (var currentIndex = 0; currentIndex < text.Length; currentIndex++)
+        {
+            var currentChar = text[currentIndex];
+            if (currentChar == '_')
+            {
+                builder.Append('_');
+                previousCategory = null;
+                continue;
+            }
+
+            var currentCategory = char.GetUnicodeCategory(currentChar);
+            switch (currentCategory)
+            {
+                case UnicodeCategory.UppercaseLetter:
+                case UnicodeCategory.TitlecaseLetter:
+                    if (previousCategory == UnicodeCategory.SpaceSeparator ||
+                        previousCategory == UnicodeCategory.LowercaseLetter ||
+                        (previousCategory != UnicodeCategory.DecimalDigitNumber &&
+                         previousCategory != null &&
+                         currentIndex > 0 &&
+                         (currentIndex + 1 < text.Length && char.IsLower(text[currentIndex + 1]))))
+                    {
+                        builder.Append('_');
+                    }
+
+                    currentChar = char.ToLower(currentChar, CultureInfo.InvariantCulture);
+                    break;
+
+                case UnicodeCategory.LowercaseLetter:
+                case UnicodeCategory.DecimalDigitNumber:
+                    if (previousCategory == UnicodeCategory.SpaceSeparator)
+                    {
+                        builder.Append('_');
+                    }
+                    break;
+
+                default:
+                    if (previousCategory != null)
+                    {
+                        previousCategory = UnicodeCategory.SpaceSeparator;
+                    }
+
+                    continue;
+            }
+
+            builder.Append(currentChar);
+            previousCategory = currentCategory;
+        }
+
+        return builder.ToString();
     }
 
     /// <summary>
