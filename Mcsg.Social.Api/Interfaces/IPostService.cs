008d19e5241dbefb96e7eff93d0884223dcb4e38
ToanNV <toannguyen@angelpj.com>
2024-08-31 12:21:56 +0700

    BUMCHEO-561 Remove code unused in PostService

--- Mcsg.Social.Api/Interfaces/IPostService.cs
+++ Mcsg.Social.Api/Interfaces/IPostService.cs
@@ -1,10 +1,8 @@
 ﻿namespace Mcsg.Social.Api.Interfaces;
 
 using Common.Core.Enums;
-using Common.Domain.Entities;
 using Common.SeedWork.Responses;
 using Dtos;
-using Enums;
 using Models;
 using Models.Earning;
 using Requests;
@@ -12,33 +10,11 @@ using Requests;
 public interface IPostService
 {
     Task<bool> Delete(Guid postId);
-    Task<PostSeriesResponse> PostSeries(PostType type, ComicPostSeriesR postReq);
-    Task<PostSeriesResponse> UpdateSeries(string hashId, ComicPostUpdateSeriesR postReq);
-    Task<PostSeriesResponse> GetSeries(string hashId, bool isLoadChapters);
-    Task<ChapterResponse> GetSeriesChapter(string hashId, int order);
-    Task<PagedResponse<ChapterResponse>> GetChapters(string hashId, ComicChapterListR request);
-    Task<PagedResponse<ChapterTOCResponse>> GetChaptersListSimple(string hashId);
     Task<PagedResponse<ChapterTOCExtendResponse>> GetChaptersListSimple(Guid userId, string hashId, ComicChapterListR loadReq);
-    Task<PagedResponse<PostSeriesTopResponse>> GetMySeries(PostType type, ComicPostListSeriesR request);
-    Task<PagedResponse<PostSeriesTopResponse>> GetTopSeriesByPage(PostType type, PostSeriesSelectedType selectedType, ComicTopPostR loadReq);
     Task<PagedResponse<PostSeriesTopResponse>> GetSeriesByTagByPage(PostType type, string tagName, ComicTopPostR loadReq);
-    Task<PagedResponse<PostSeriesTopResponse>> GetSeriesByUserByPage(PostType type, string profileName, ComicTopPostR loadReq);
-    Task<List<PostSeriesTopResponse>> GetTopNewSeries(PostType type, int number);
-    Task<PostSeriesAllTopResponse> GetTopSeries(PostType type);
-    Task<PagedResponse<PostSeriesTopResponse>> GetTopSeriesAsync(PostType type, ComicPostListSeriesR request);
-    Task<PagedResponse<PostSeriesTopResponse>> GetRelationSeriesAsync(PostType type, ComicRelationPostSeriesR request);
-
-    Task<SocialSubPost> SubPostChapterToSeries(string hashId, StoryChapterPostR chapterPostReq);
-    Task<SocialSubPost> SubPostUpdateChapterToSeries(string hashId, int order, StoryChapterPostR chapterPostReq);
-    Task<List<ChapterResponse>> SwapChapterOrder(string comicHashId, ComicChapterOrderSwapR orders);
-    Task<bool> DeleteChapter(string hashId, int order);
-    void VerifyBasicInfo(string title);
-    ChapterResponse MappingChapterResponse(SocialSubPost newChapter);
     Task<List<RewardDto>> CheckRewardsForPost(Guid currentUserId, PostType type);
     Task<List<MyPostSeriesResponse>> GetMyAllSeries();
     Task UpdateKeyWordForComicAndStoryToSmartLookup();
-    Task<PagedResponse<PostBoxResposne>> GetPostByUserProfileName(PostType type, ComicPostByProFileNameR input);
-    Task<PagedResponse<PostBoxResposne>> GetPostByTagName(PostType type, ComicPostByTagNameR input);
     Task<IEnumerable<string>> GetPostRandomIdsAsync(PostRandomIdsR req);
     Task<ListIdForHomePage> GetLatestPostsByType();
     Task<ListIdForHomePage> GetLatestPostsByTag(string nameTag);
