1b28b8a7daf243b5196ade5a32308ea3eb29dc85
ToanNV <toannguyen@angelpj.com>
2024-09-07 15:43:23 +0700

    Add missing -- Load settings --

--- Mcsg.Media.Tool/Program.cs
+++ Mcsg.Media.Tool/Program.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Mcsg.Media.Tool;
 
 using Common.Core.Extensions;
 using Common.Core.Interfaces;
+using Common.Domain;
 using Common.SeedWork.Extensions;
 using static Common.SeedWork.Constants.Setting;
 
@@ -26,6 +28,10 @@ internal class Program
             .AddEnvironmentVariables()
             .AddCommandLine(args);
 
+        // Get assembly name
+        var me = typeof(Program);
+        var assembly = me.Assembly.GetName().Name;
+
         // Load settings from the environment
         var st = _prefix.ConvertEnvironmentVariable<Setting>(CommonPrefix);
         st.Prefix = _prefix;
@@ -37,12 +43,26 @@ internal class Program
 
         var services = new ServiceCollection();
 
+        // DbContext
+        services.AddDbContext<McsgContext>(p => p.UseNpgsql(st.DefaultConnection, p => p.MigrationsAssembly(assembly).EnableRetryOnFailure()), ServiceLifetime.Scoped);
+        services.AddScoped<IMcsgContext>(p => p.GetService<McsgContext>()!);
+
         // Storage
         st.LoadStorages();
         services.AddStorage(p => { p.Storages = st.Minio.Storages; });
 
         var serviceProvider = services.BuildServiceProvider();
         var sc = serviceProvider.GetService<IStorageClient>();
+
+        #region -- Load settings --
+        using (var ss = serviceProvider.GetRequiredService<IServiceScopeFactory>().CreateScope())
+        {
+            var context = ss.ServiceProvider.GetRequiredService<IMcsgContext>();
+            var dic = context.SystemSettings.Where(p => !string.IsNullOrWhiteSpace(p.Key)).ToDictionary(p => p.Key + "", p => p.Value + "");
+
+            st.LoadApiUrl(dic, st.IsLocal, !string.IsNullOrWhiteSpace(st.Protocols));
+        }
+        #endregion
 
         var cancellation = new CancellationTokenSource();
         await new WorkDistributor(st, sc!).Run(cancellation.Token);
