9044035d3a5ebd5c7c25f32648a2aea6ebb066d7
TanPM <korristan@angelpj.com>
2024-09-19 15:46:29 +0700

    BUMCHEO-692 change int to float request params order swap

--- Mcsg.Story.Api/Requests/Stories/StoryChapterOrderSwapR.cs
+++ Mcsg.Story.Api/Requests/Stories/StoryChapterOrderSwapR.cs
@@ -2,6 +2,6 @@
 
 public class StoryChapterOrderSwapR
 {
-    public int Order1 { get; set; }
-    public int Order2 { get; set; }
+    public float Order1 { get; set; }
+    public float Order2 { get; set; }
 }
