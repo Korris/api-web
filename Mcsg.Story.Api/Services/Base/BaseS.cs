4d4a586e054da93a62f05a23ac0ce3e46cb7eb9c
ToanNV <toannguyen@angelpj.com>
2024-09-24 16:43:04 +0700

    BUMCHEO-705 Add BaseS

---
+++ Mcsg.Story.Api/Services/Base/BaseS.cs
@@ -0,0 +1,43 @@
+﻿namespace Mcsg.Story.Api.Services;
+
+using Common.Domain;
+using Interfaces;
+
+/// <summary>
+/// Base service
+/// </summary>
+/// <remarks>
+/// Initialize
+/// </remarks>
+/// <param name="context">DB context</param>
+public class BaseS(IMcsgContext context)
+{
+    #region -- Fields --
+
+    /// <summary>
+    /// DB context
+    /// </summary>
+    protected readonly IMcsgContext _context = context;
+
+    #endregion
+}
+
+/// <summary>
+/// Base service
+/// </summary>
+/// <remarks>
+/// Initialize
+/// </remarks>
+/// <param name="context">DB context</param>
+/// <param name="setting">Setting</param>
+public class BaseSettingS(IMcsgContext context, ISetting setting) : BaseS(context)
+{
+    #region -- Fields --
+
+    /// <summary>
+    /// Setting
+    /// </summary>
+    protected readonly ISetting _setting = setting;
+
+    #endregion
+}
---
+++ Mcsg.Wallet.Api/Services/Base/BaseS.cs
@@ -0,0 +1,43 @@
+﻿namespace Mcsg.Wallet.Api.Services;
+
+using Domain;
+using Interfaces;
+
+/// <summary>
+/// Base service
+/// </summary>
+/// <remarks>
+/// Initialize
+/// </remarks>
+/// <param name="context">DB context</param>
+public class BaseS(WalletContext context)
+{
+    #region -- Fields --
+
+    /// <summary>
+    /// DB context
+    /// </summary>
+    protected readonly WalletContext _context = context;
+
+    #endregion
+}
+
+/// <summary>
+/// Base service
+/// </summary>
+/// <remarks>
+/// Initialize
+/// </remarks>
+/// <param name="context">DB context</param>
+/// <param name="setting">Setting</param>
+public class BaseSettingS(WalletContext context, ISetting setting) : BaseS(context)
+{
+    #region -- Fields --
+
+    /// <summary>
+    /// Setting
+    /// </summary>
+    protected readonly ISetting _setting = setting;
+
+    #endregion
+}
