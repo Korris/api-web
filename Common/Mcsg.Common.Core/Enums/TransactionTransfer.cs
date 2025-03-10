d3a97c886d405b193235bb1b11eef6770bfec235
ToanNV (home) <nvt87x@gmail.com>
2025-03-10 14:14:13 +0700

    Sync from bumcheo-api-wallet

---
+++ Common/Mcsg.Common.Core/Enums/TransactionTransfer.cs
@@ -0,0 +1,27 @@
+﻿namespace Mcsg.Common.Core.Enums;
+
+/// <summary>
+/// TransactionTransfer
+/// </summary>
+public enum TransactionTransfer
+{
+    /// <summary>
+    /// Trasfer
+    /// </summary>
+    Trasfer,
+
+    /// <summary>
+    /// Receive
+    /// </summary>
+    Receive,
+
+    /// <summary>
+    /// SendDonate
+    /// </summary>
+    Donate,
+
+    /// <summary>
+    /// ReceiveDonate
+    /// </summary>
+    ReceiveDonate
+}
