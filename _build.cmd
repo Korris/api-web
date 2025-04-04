229cade8a869a60c2a94b2719390ab82a59363ef
ToanNV <toannguyen@ntada.vn>
2025-04-04 10:44:07 +0700

    Fail the build if the zip file is smaller than 10KB

--- _build.cmd
+++ _build.cmd
@@ -23,4 +23,20 @@ for /f "tokens=1-2 delims=: " %%i in ('time /t') do set mytime=%%i-%%j
 echo Focfoc-%1.%3-1.0.%BUILD_NUMBER% %mydate% %mytime% > version.txt
 
 :: 5. Compress the publish folder
-7z a -tzip %ZipFile% .\*
+7z a -tzip "%ZipFile%" .\*
+if errorlevel 1 (
+  echo Compression failed
+  exit /b 1
+)
+
+:: 6. Check the size of the zip file
+for %%F in (%ZipFile%) do set filesize=%%~zF
+
+:: 7. Fail the build if the zip file is smaller than 10KB (10240 bytes)
+if %filesize% LSS 10240 (
+  echo Build failed: Zip file is smaller than 10KB.
+  exit /b 1
+)
+
+echo Build and packaging completed successfully
+exit /b 0
